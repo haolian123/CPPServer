b632b2f1225c26055671000aa53455afe6073cef
Haolian Huang <1263164021@qq.com>
2023-12-15 18:57:14 +0800

    修复空指针异常和添加注释

--- ThreadPool.h
+++ ThreadPool.h
@@ -1,37 +1,46 @@
 #ifndef THREADPOOL
 #define THREADPOOL
+
 #include <mutex>
 #include <condition_variable>
 #include <queue>
 #include <thread>
-#include<cassert>
+#include <cassert>
 #include <functional>
-struct Pool{
-    std::mutex mutex;
-    std::condition_variable condition;
-    bool isClose;
-    std::queue<std::function<void()>> tasks;
+
+// 线程池结构体，用于管理任务队列和线程状态
+struct Pool {
+    std::mutex mutex;                    // 互斥锁，用于保护任务队列
+    std::condition_variable condition;    // 条件变量，用于线程等待任务
+    bool isClose;                        // 线程池是否关闭
+    std::queue<std::function<void()>> tasks; // 任务队列，存放需要执行的任务
 };
-class ThreadPool{
+
+class ThreadPool {
 private:
-    std::shared_ptr<Pool>pool;
+    std::shared_ptr<Pool> pool; // 线程池结构体的共享指针
+
 public:
-    explicit ThreadPool(size_t threadNumber=8):pool(std::make_shared<Pool>()){
-        assert(threadNumber>0);
-        for(size_t i=0;i<threadNumber;i++){
+    // 构造函数，创建线程池并启动线程
+    explicit ThreadPool(size_t threadNumber = 8)
+        : pool(std::make_shared<Pool>()) {
+        assert(threadNumber > 0);
+
+        // 创建指定数量的线程，并将它们绑定到任务执行函数
+        for (size_t i = 0; i < threadNumber; i++) {
             std::thread(
-                [pool=this->pool]{
-                    std::unique_lock<std::mutex>locker(pool->mutex);
-                    while(true){
-                        if(!pool->tasks.empty()){
-                            auto task=std::move(pool->tasks.front());
+                [pool = this->pool] {
+                    std::unique_lock<std::mutex> locker(pool->mutex);
+                    while (true) {
+                        if (!pool->tasks.empty()) {
+                            auto task = std::move(pool->tasks.front());
                             pool->tasks.pop();
                             locker.unlock();
                             task();
                             locker.lock();
-                        }else if(pool->isClose){
+                        } else if (pool->isClose) {
                             break;
-                        }else{
+                        } else {
                             pool->condition.wait(locker);
                         }
                     }
@@ -39,30 +48,31 @@ public:
             ).detach();
         }
     }
-    ThreadPool()=default;
+
+    // 默认构造函数
+    ThreadPool() = default;
+
+    // 移动构造函数
     ThreadPool(ThreadPool&&) = default;
 
-    ~ThreadPool(){
-        if(pool!=nullptr){
-            std::lock_guard<std::mutex>locker(pool->mutex);
-            pool->isClose=true;
+    // 析构函数，关闭线程池
+    ~ThreadPool() {
+        if (pool != nullptr) {
+            std::lock_guard<std::mutex> locker(pool->mutex);
+            pool->isClose = true;
         }
         pool->condition.notify_all();
     }
-    template<class F>
-    void addTask(F&& task){
+
+    // 添加任务到任务队列
+    template <class F>
+    void addTask(F&& task) {
         {
-            std::lock_guard<std::mutex>locker(pool->mutex);
+            std::lock_guard<std::mutex> locker(pool->mutex);
             pool->tasks.emplace(std::forward<F>(task));
         }
         pool->condition.notify_one();
     }
 };
 
-
-
-
-
-
-
-#endif 
+#endif
