576275fa5ebb038eb3caa9d09ef685a2593f41ef
Haolian Huang <1263164021@qq.com>
2023-12-15 18:55:08 +0800

    epoll管理类

--- EpollManager.h
+++ EpollManager.h
@@ -10,26 +10,35 @@
 #include<cassert>
 #define MAXEVENT 1024
 
-class EpollManager{
+class EpollManager {
 private:
-    int epollFd;
-    std::vector<struct epoll_event> events;   
+    int epollFd;                            // epoll 文件描述符
+    std::vector<struct epoll_event> events;  // epoll 事件集合
+
 public:
-    EpollManager(int maxEvent=MAXEVENT);
+    // 构造函数，默认参数为最大事件数 MAXEVENT
+    EpollManager(int maxEvent = MAXEVENT);
+
+    // 析构函数
     ~EpollManager();
 
+    // 添加文件描述符和事件到 epoll 实例中
     bool addFd(int fd, uint32_t events);
 
+    // 修改文件描述符的关注事件
     bool modifyFd(int fd, uint32_t events);
 
+    // 从 epoll 实例中删除文件描述符
     bool deleteFd(int fd);
 
-    int wait(int timeout=-1);
+    // 等待 epoll 事件的发生，可指定超时时间
+    int wait(int timeout = -1);
 
+    // 获取指定事件的文件描述符
     int getEventFd(size_t i) const;
 
+    // 获取指定事件的事件类型
     uint32_t getEvents(size_t i) const;
-
 };
 
 #endif 
