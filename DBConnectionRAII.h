8a14929000dcf667919a86590fb93b2fad83c2c7
Haolian Huang <1263164021@qq.com>
2023-12-15 18:54:43 +0800

    数据库连接RAII范式

--- DBConnectionRAII.h
+++ DBConnectionRAII.h
@@ -2,22 +2,26 @@
 #define DB_CONNECTION_RAII 
 #include"DBConnectionPool.h"
 
-class DBConnectionRAII{
+class DBConnectionRAII {
 public:
-    DBConnectionRAII(MYSQL*&sql,DBConnectionPool*pool){
+    // 构造函数，获取数据库连接
+    DBConnectionRAII(MYSQL*& sql, DBConnectionPool* pool) {
         assert(pool);
-        sql=pool->getConnection();
-        this->sql=sql;
-        connectionPool=pool;
+        sql = pool->getConnection();
+        this->sql = sql;
+        connectionPool = pool;
     }
-    ~DBConnectionRAII(){
-        if(sql){
+
+    // 析构函数，释放数据库连接
+    ~DBConnectionRAII() {
+        if (sql) {
             connectionPool->freeConnection(sql);
         }
     }
+
 private:
-    MYSQL* sql;
-    DBConnectionPool* connectionPool;
+    MYSQL* sql;                    // 数据库连接
+    DBConnectionPool* connectionPool; // 连接池指针
 };
 
 
